7048ce2c38655faee218bc88007426376e41f8c4
idle sign <idlesign@yandex.ru>
2018-08-01 20:38:24 +0700

    Bodies titles are now have commas.

--- vininfo/dicts/bodies.py
+++ vininfo/dicts/bodies.py
@@ -2,23 +2,23 @@
 from __future__ import unicode_literals
 
 
-BODY_CABRI_2 = 'Cabriolet 2-Door'
-BODY_COUPE_2 = 'Coupe 2-Door'
-BODY_CROSS_3 = 'Crossover 3-Door'
-BODY_PICKUP_2 = 'Pickup 2-Door'
+BODY_CABRI_2 = 'Cabriolet, 2-Door'
+BODY_COUPE_2 = 'Coupe, 2-Door'
+BODY_CROSS_3 = 'Crossover, 3-Door'
+BODY_PICKUP_2 = 'Pickup, 2-Door'
 
-BODY_HATCH_3 = 'Hatchback 3-Door'
-BODY_HATCH_5 = 'Hatchback 5-Door'
+BODY_HATCH_3 = 'Hatchback, 3-Door'
+BODY_HATCH_5 = 'Hatchback, 5-Door'
 
-BODY_MINIVAN_3 = 'Minivan 3-Door'
-BODY_MINIVAN_5 = 'Minivan 5-Door'
+BODY_MINIVAN_3 = 'Minivan, 3-Door'
+BODY_MINIVAN_5 = 'Minivan, 5-Door'
 
-BODY_SEDAN_4 = 'Sedan 4-Door'
-BODY_SEDAN_2 = 'Sedan 2-Door'
+BODY_SEDAN_4 = 'Sedan, 4-Door'
+BODY_SEDAN_2 = 'Sedan, 2-Door'
 
-BODY_SW_3 = 'Station Wagon 3-Door'
-BODY_SW_5 = 'Station Wagon 5-Door'
-BODY_SW_8 = 'Station Wagon 8-Door'
+BODY_SW_3 = 'Station Wagon, 3-Door'
+BODY_SW_5 = 'Station Wagon, 5-Door'
+BODY_SW_8 = 'Station Wagon, 8-Door'
 
 BODY_MINIBUS = 'Minibus'
 BODY_VAN = 'Van'
