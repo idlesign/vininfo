cfb74155f8fbd19e0bcd27272f3b560c572c4948
idle sign <idlesign@yandex.ru>
2020-05-02 20:17:46 +0700

    Release v1.4.0

--- vininfo/__init__.py
+++ vininfo/__init__.py
@@ -2,7 +2,7 @@ from .toolbox import Vin
 from .exceptions import ValidationError, VininfoException
 
 
-VERSION = (1, 3, 0)
+VERSION = (1, 4, 0)
 """Application version number tuple."""
 
 VERSION_STR = '.'.join(map(str, VERSION))
